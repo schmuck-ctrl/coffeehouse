645d3b6f3e24bf38ceb19b3e2191781b86129ff8
schmuck-ctrl <nils.schmuck@detim.de>
2025-06-05 18:27:25 +0200

    adding sample data

---
+++ coffeesize/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+use coffeesize;
+
+INSERT INTO coffee_size (id, name, short_description)
+VALUES ('1', 'small', 's');
+
+INSERT INTO coffee_size (id, name, short_description)
+VALUES ('2', 'medium', 'm');
+
+INSERT INTO coffee_size (id, name, short_description)
+VALUES ('3', 'large', 'l');
+
+INSERT INTO coffee_size (id, name, short_description)
+VALUES ('4', 'extra large', 'xl');
---
+++ coffeetype/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+use coffeetype;
+
+insert into coffee_type (id, name)
+VALUES ('1', 'Kaffee');
+
+insert into coffee_type (id, name)
+VALUES ('2', 'Espresso');
+
+insert into coffee_type (id, name)
+VALUES ('3', 'Cappuccino');
+
+insert into coffee_type (id, name)
+VALUES ('4', 'Flat White');
